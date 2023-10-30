ee36f306d05c35924b551f7749088272dfcd47ec
Adonai Solomon <solomonadonay1@gmail.com>
2023-10-30 14:54:21 +0300

    Update gretting.py

--- plugins/gretting.py
+++ plugins/gretting.py
@@ -4,5 +4,5 @@ from pyrogram.types import *
 @neural.on_message( filters.command("start"))
 async def start(bot, msg):
     markup = InlineKeyboardMarkup([[channel, group], [developer,help]])
-    await msg.reply(f"**Hello {msg.from_user.first_name} I'm ChatGPT turbo 3.5 open source Telegram Bot powered by Neurals,you can ask me anything\nclick help for more **",
+    await msg.reply(f"**Hello {msg.from_user.first_name} I'm ChatGPT turbo 3.5 open source Telegram Bot, you can ask me anything\nclick help for more **",
     reply_markup= markup )
