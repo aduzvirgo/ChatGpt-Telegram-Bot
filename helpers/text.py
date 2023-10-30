268dc5f130c24f8f7ec67e64b8b744fae56d71b7
Adonai Solomon <solomonadonay1@gmail.com>
2023-10-30 14:58:26 +0300

    Update text.py

--- helpers/text.py
+++ helpers/text.py
@@ -1,9 +1,9 @@
 HELP = """**--•Examples--\n \n\n• Explain quantum computing in simple terms
 • Got any creative ideas for a 10 year olds birthday?
 • How do I make an HTTP request in Javascript?
-\n\n•¸Capabilites\n\n•Remembers what user said earlier in the conversation,
+\n\n•Capabilites\n\n•Remembers what user said earlier in the conversation,
 •A llows user to provide follow-up corrections,
-• Trained to decline inappropriate requests\n\n•Limitations\n\nMay occasionally generate incorrect information,
+• Trained to decline inappropriate requests\n\n•Limitations\n\n• May occasionally generate incorrect information,
 • May occasionally produce harmful instructions or biased content,
 • Limited knowledge of world and events after 2021
 • may it takes to long for some responses the messages $hello and $genrate me some c++ code can not be treated the same,so depending on your query bot can take  min 3second and max 30 seconds to response
