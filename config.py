870ddf4ab3b435afdbed4961891abd90e78266ef
Adonai Solomon <solomonadonay1@gmail.com>
2023-10-30 18:08:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #pass your tokens here 
-BOT_TOKEN ='6172564028:AAEe0l6kn1YUM2j4bj5SvDNT37kh3cMnX1Y' # bot token from t.me/botfather
+BOT_TOKEN ='6172564028:AAG4xV8CfFuMzyn4oRPsquab3X1PdRiEZOM' # bot token from t.me/botfather
 API_ID = 28153993 #api id from telegram.org
 API_HASH = '976fd7cc4958ad84181a53b41919564b' #api hash from telegram.org
     
