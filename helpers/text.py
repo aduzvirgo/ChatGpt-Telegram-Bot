1966f0684c127b63db731bedf007488a678f4be0
Ethio Ai <solomonadonay1@gmail.com>
2023-11-01 20:42:58 +0300

    Update text.py

--- helpers/text.py
+++ helpers/text.py
@@ -1,15 +1,12 @@
-HELP = """**--•Examples--\n \n\n• Explain quantum computing in simple terms
-• Got any creative ideas for a 10 year olds birthday?
-• How do I make an HTTP request in Javascript?
-\n\n•Capabilites\n\n•Remembers what user said earlier in the conversation,
-•A llows user to provide follow-up corrections,
-• Trained to decline inappropriate requests\n\n•Limitations\n\n• May occasionally generate incorrect information,
-• May occasionally produce harmful instructions or biased content,
-• Limited knowledge of world and events after 2021
-• may it takes to long for some responses the messages $hello and $genrate me some c++ code can not be treated the same,so depending on your query bot can take  min 3second and max 30 seconds to response
-
-source Code https://github.com/ethioaiofficial/ChatGpt-Telegram-Bot
-**"""
+HELP = """**type /help for this menu\n\ntype /mysetting to see your current setting 
+    \n\ntype /language to set language
+    \n\ntype /tone to set approach/tone 
+    \n\n type /apikey to get your own http Api key
+    
+    \n \n\n or directly ask a question
+    + works inline everywhere 
+    
+    \n \n\n to upgrade your Api for commercial use, contact us @neuralg or DM @aduzvirgo**"""
 
 
 
