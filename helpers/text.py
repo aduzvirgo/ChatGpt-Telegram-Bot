25fcb906f6c1e1bbdc0f745027ce69bbdf2fe996
Paradox <solomonadonay1@gmail.com>
2023-12-03 11:13:04 +0300

    Update text.py

--- helpers/text.py
+++ helpers/text.py
@@ -1,15 +1,12 @@
-HELP = """**type /help for this menu\n\ntype /mysetting to see your current setting 
-    \n\ntype /language to set language
-    \n\ntype /tone to set approach/tone 
-    \n\n type /apikey to get your own http Api key
-    
-    \n \n\n or directly ask a question
-    + works inline everywhere 
-    
-    \n \n\n to upgrade your Api for commercial use, contact us @ethioaiofficial or DM @aduzvirgo**"""
+HELP = """**--•*Examples*--\n \n\n•Explain quantum computing in simple terms\n
+•Got any creative ideas for a 10 year oldâ€™s birthday?\n
+•How do I make an HTTP request in Javascript?
+\n\n•*Capabilites*\n\n•Remembers what user said earlier in the conversation,\n
+•Allows user to provide follow-up corrections,\n
+•Trained to decline inappropriate requests\n\n•*Limitations*\n\n•May occasionally generate incorrect information,\n
+• May occasionally produce harmful instructions or biased content,\n
+•Limited knowledge of world and events after 2021\n
+•May it takes to long for some responses the messages $hello and $genrate me some c++ code can not be treated the same,so depending on your query bot can take  min 3second and max 30 seconds to response. 
 
-
-
-
-
-
+➡️ *source Code* https://github.com/aduzvirgo/ChatGpt-Telegram-Bot
+**"""
