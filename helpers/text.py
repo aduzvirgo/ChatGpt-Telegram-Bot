2d0d2161edd9a945501b544755b9ce96df9b8c9b
Paradox <solomonadonay1@gmail.com>
2023-12-03 11:16:33 +0300

    Update text.py

--- helpers/text.py
+++ helpers/text.py
@@ -1,12 +1,12 @@
-HELP = """**--•*Examples*--\n \n\n•Explain quantum computing in simple terms\n
-•Got any creative ideas for a 10 year oldâ€™s birthday?\n
-•How do I make an HTTP request in Javascript?
-\n\n•*Capabilites*\n\n•Remembers what user said earlier in the conversation,\n
-•Allows user to provide follow-up corrections,\n
-•Trained to decline inappropriate requests\n\n•*Limitations*\n\n•May occasionally generate incorrect information,\n
+HELP = """**--Examples--\n \n\n• Explain quantum computing in simple terms\n
+• Got any creative ideas for a 10 year oldâ€™s birthday?\n
+• How do I make an HTTP request in Javascript?
+\n\nCapabilites\n\n• Remembers what user said earlier in the conversation,\n
+• Allows user to provide follow-up corrections,\n
+• Trained to decline inappropriate requests\n\nLimitations\n\n• May occasionally generate incorrect information,\n
 • May occasionally produce harmful instructions or biased content,\n
-•Limited knowledge of world and events after 2021\n
-•May it takes to long for some responses the messages $hello and $genrate me some c++ code can not be treated the same,so depending on your query bot can take  min 3second and max 30 seconds to response. 
+• Limited knowledge of world and events after 2021\n
+• May it takes to long for some responses the messages $hello and $genrate me some c++ code can not be treated the same,so depending on your query bot can take  min 3second and max 30 seconds to response. 
 
-➡️ *source Code* https://github.com/aduzvirgo/ChatGpt-Telegram-Bot
+➡️ Source Code https://github.com/aduzvirgo/ChatGpt-Telegram-Bot
 **"""
