3f526202fc660a628098019d7a2eaaf874d18cf8
Paradox <solomonadonay1@gmail.com>
2024-01-12 18:42:46 +0300

    Update buttons.py

--- helpers/buttons.py
+++ helpers/buttons.py
@@ -1,9 +1,9 @@
 from pyrogram.types import InlineKeyboardButton as ikb
 
 
-channel = ikb("Channel🌴",url='https://t.me/ethioaiofficial')
-group = ikb("Group🪺",url='https://t.me/ethioaichat')
-developer = ikb("Developer🦭",url="https://t.me/aduzvirgo")
+channel = ikb("Channel🌴",url='https://t.me/TigrayTechc')
+group = ikb("Group🪺",url='https://t.me/tigraytechs')
+developer = ikb("Developer🦭",url="https://t.me/snackshell")
 help = ikb("Help📃",callback_data='help')
 
 
