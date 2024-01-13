c05e5feea2a9145e5423c4715ab55e93a89a4e4a
Paradox <solomonadonay1@gmail.com>
2024-01-13 18:26:25 +0300

    Update gpt.py

--- helpers/gpt.py
+++ helpers/gpt.py
@@ -2,7 +2,7 @@
 import requests
 
 def gpt(text):
-    url = "https://freegptapi.hop.sh/neural/api"
+    url = "http://216.98.10.228:5000/gpt"
     params = {
     "query":text}
     response = requests.get(url, params=params)
