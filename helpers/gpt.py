609ab6cf3d46c55987dd27270b1749c5e6861a2e
Paradox <solomonadonay1@gmail.com>
2024-02-08 21:13:35 +0300

    Update gpt.py

--- helpers/gpt.py
+++ helpers/gpt.py
@@ -1,7 +1,7 @@
 import requests
 
 def gpt(text):
-    url = "http://62.72.6.182:6699/gpt"
+    url = "http://62.72.6.182:6699/gpt?ask=Hi"
     params = {"ask": text}
     response = requests.get(url, params=params)
     data = response.json()
