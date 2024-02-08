aebfe794216764170339833b877d0977d927bc33
Paradox <solomonadonay1@gmail.com>
2024-02-08 21:25:04 +0300

    Update gpt.py

--- helpers/gpt.py
+++ helpers/gpt.py
@@ -1,15 +1,8 @@
 import requests
 
 def gpt(text):
-    url = "http://62.72.6.182:6699/gpt?ask=Hi"
+    url = "http://62.72.6.182:6699/gpt"
     params = {"ask": text}
-
-    try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        print("HTTP Status Code:", response.status_code)  # Print the status code
-        data = response.json()
-        return data['answer']
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return None
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data['answer']
