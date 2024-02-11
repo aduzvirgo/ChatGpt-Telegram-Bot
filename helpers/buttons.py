3153e52a73d7d74f0a60f9a3065c6dfa75593b0f
Adonay <solomonadonay1@gmail.com>
2024-02-11 21:11:12 +0300

    Update buttons.py

--- helpers/buttons.py
+++ helpers/buttons.py
@@ -1,8 +1,8 @@
 from pyrogram.types import InlineKeyboardButton as ikb
 
 
-channel = ikb("Channel🌴",url='https://t.me/TigrayTechc')
-group = ikb("Group🪺",url='https://t.me/tigraytechs')
+channel = ikb("Channel🌴",url='https://t.me/ethioaitechnology')
+group = ikb("Group🪺",url='https://t.me/ethioaicommunity')
 developer = ikb("Developer🦭",url="https://t.me/snackshell")
 help = ikb("Help📃",callback_data='help')
 
