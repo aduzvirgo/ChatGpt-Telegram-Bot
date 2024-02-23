7e3a0f3d7cb27676e1d54404b68e421691df5fb9
Adonay <solomonadonay1@gmail.com>
2024-02-23 22:49:26 +0300

    Update gpt.py

--- helpers/gpt.py
+++ helpers/gpt.py
@@ -1,11 +1,9 @@
 import requests
 
-url = "https://freegptapi.hop.sh/neural/api"
-params = {
-    "query": "hello world"
-}
-
-response = requests.get(url, params=params)
-data = response.json()
-
-print(data)
+def gpt(hello world):
+    url = "https://freegptapi.hop.sh/neural/api"
+    params = {
+    "query": "hello world"}
+    response = requests.get(url, params=params)
+    data = response.json()
+    print(data)
