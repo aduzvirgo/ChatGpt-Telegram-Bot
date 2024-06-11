68be9da357af0534f54bbb058071333b50047625
paradox <solomonadonay1@gmail.com>
2024-06-11 12:27:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #pass your tokens here 
-BOT_TOKEN ='6918482821:AAGaJxW26PFlXzf41UHZ7fJiSIZCtDU8uec' # bot token from t.me/botfather
+BOT_TOKEN ='6918482821:AAEvENyy7mps0HpAzFmjBohE0GGBKqus8E0' # bot token from t.me/botfather
 API_ID = 28153993 #api id from telegram.org
 API_HASH = '976fd7cc4958ad84181a53b41919564b' #api hash from telegram.org
     
